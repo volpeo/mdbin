44899ed00e727338b4f308e2043c3e001fc563ed
Sylvain Peigney <sylvain@volpeo.fr>
2015-12-11 18:18:37 +0100

    about modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,122 +4,143 @@
 //= require simplemde
 //= require_tree .
 
+'use strict';
 
-$(document).ready(function(){
-  if ($('body').is('.bins_new, .bins_edit')) {
-    var simplemde = new SimpleMDE({
-      element: $("#simplemde")[0],
-      spellChecker: false,
-      autofocus: true,
-      indentWithTabs: true,
-      status: false,
-      toolbar: [
-      "bold",
-      "italic",
-      "heading",
-      "|",
-      "quote",
-      "code",
-      "unordered-list",
-      "ordered-list",
-      "|",
-      {
-        name: "URL",
-        action: function customFunction(editor){
-          $('#linkModal').modal();
-          $('#insertLink').off('click').on('click', function() {
-            $('#linkModal').modal('hide');
-            var url = $('#linkUrl').val();
-            $('#linkUrl').val('');
-            var title = $('#linkTitle').val();
-            $('#linkTitle').val('');
-            var cm = editor.codemirror;
-            var doc = cm.getDoc();
-            var cursor = doc.getCursor(); // gets the line number in the cursor position
-            var line = doc.getLine(cursor.line); // get the line contents
-            var pos = { // create a new object to avoid mutation of the original selection
-              line: cursor.line,
-              ch: line.length // set the character position to the end of the line
-            }
-            var anchor = "["+title+"]("+url+")"
-            console.log("je suis dedans")
-            doc.replaceRange(anchor, pos); // adds a new line
-          });
-        },
-        className: "fa fa-link",
-        title: "link"
-      },
-      {
-        name: "Image",
-        action: function customFunction(editor){
-          $('#imgModal').modal();
-          $('#insertImg').off('click').on('click', function() {
-            $('#imgModal').modal('hide');
-            var url = $('#imgUrl').val();
-            var alt = $('#imgAlt').val();
-            var cm = editor.codemirror;
-            var doc = cm.getDoc();
-            var cursor = doc.getCursor(); // gets the line number in the cursor position
-            var line = doc.getLine(cursor.line); // get the line contents
-            var pos = { // create a new object to avoid mutation of the original selection
-              line: cursor.line,
-              ch: line.length - 1 // set the character position to the end of the line
-            }
-            var anchor = "!["+alt+"]("+url+")"
-            doc.replaceRange(anchor, pos); // adds a new line
-          });
-        },
-        className: "fa fa-image",
-        title: "image"
-      },
-      "|",
-      "preview",
-      "side-by-side",
-      "guide"
-      ]
-    });
+$(document).ready(function() {
 
+  var SAVED = true;
 
-
-    simplemde.toggleSideBySide();
-    simplemde.toggleSideBySide();
-
-    document.addEventListener("keydown", function(e) {
-      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-        e.preventDefault();
-        loadSaveFlash();
-        $('#commitBin').trigger('click');
-      }
-    }, false);
-
-    $('#saveBin').on('click', function(e) {
-      e.preventDefault();
-      loadSaveFlash();
-      $('#commitBin').trigger('click');
-    });
-
-    var loadSaveFlash = function () {
-      // cloner le flash de save
-      var $flashSave = $('#flash-save').clone()
-        .removeAttr('id')
-        .removeClass('hidden')
-        .addClass('alert-to-delete');
-      // le append
-      $(".flashes").first().append($flashSave);
-    }
-
-    $('#forkBin').on('click', function(e) {
-      e.preventDefault();
-      // alter the action route to new
-      $('#theBin').attr('action', '/');
-      $('#theBin').find('[name="_method"]').attr('value', 'post');
-      $('#commitBin').trigger('click');
-
-    });
-
-    setTimeout(function() {
-      $('.loading').fadeOut('slow')
-    }, 500);
+  if (!$('body').is('.bins_new, .bins_edit')) {
+    return;
   }
+  var insertInto = function(editor, item){
+    var cm = editor.codemirror;
+    var doc = cm.getDoc();
+    var cursor = doc.getCursor();
+    var line = doc.getLine(cursor.line);
+    var pos = {
+      line: cursor.line,
+      ch: line.length - 1
+    };
+    doc.replaceRange(item, pos);
+  };
+  var simplemde = new SimpleMDE({
+    element: $("#simplemde")[0],
+    spellChecker: false,
+    autofocus: true,
+    indentWithTabs: true,
+    status: false,
+    toolbar: [
+    "bold",
+    "italic",
+    "heading",
+    "|",
+    "quote",
+    "code",
+    "unordered-list",
+    "ordered-list",
+    "|",
+    {
+      name: "URL",
+      action: function customFunction(editor){
+        $('#linkModal').modal();
+        $('#insertLink').off('click').on('click', function() {
+          $('#linkModal').modal('hide');
+          var url = $('#linkUrl').val();
+          $('#linkUrl').val('');
+          var title = $('#linkTitle').val();
+          $('#linkTitle').val('');
+          var link = "["+title+"]("+url+")";
+          var cm = editor.codemirror;
+          insertInto(editor, link)
+        });
+      },
+      className: "fa fa-link",
+      title: "Insert link"
+    },
+    {
+      name: "Image",
+      action: function customFunction(editor){
+        $('#imgModal').modal();
+        $('#insertImg').off('click').on('click', function() {
+          $('#imgModal').modal('hide');
+          var url = $('#imgUrl').val();
+          var alt = $('#imgAlt').val();
+          $('#imgUrl').val('');
+          $('#imgAlt').val('');
+          var image = "!["+alt+"]("+url+")";
+          insertInto(editor, image);
+        });
+      },
+      className: "fa fa-image",
+      title: "Insert image"
+    },
+    "|",
+    "preview",
+    "side-by-side",
+    {
+      name: "About",
+      action: function customFunction(editor){
+        $('#aboutModal').modal();
+      },
+      className: "fa fa-question-circle",
+      title: "About"
+    },
+    ]
+  });
 
-})
+  simplemde.codemirror.on("change", function(){
+    SAVED = false;
+  });
+
+  // do it twice you maggot
+  simplemde.toggleSideBySide();
+  simplemde.toggleSideBySide();
+
+  var loadSaveFlash = function () {
+    var $flashSave = $('#flash-save').clone()
+      .removeAttr('id')
+      .removeClass('hidden')
+      .addClass('alert-to-delete');
+    $(".flashes").first().append($flashSave);
+  };
+
+  var saveBin = function() {
+    loadSaveFlash();
+    $('#commitBin').trigger('click');
+    SAVED = true;
+  };
+
+  document.addEventListener("keydown", function(e) {
+    if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      e.preventDefault();
+      saveBin();
+    }
+  }, false);
+
+  $('#saveBin').on('click', function(e) {
+    e.preventDefault();
+    saveBin();
+  });
+
+  $('#forkBin').on('click', function(e) {
+    e.preventDefault();
+    // alter the action route to new
+    $('#theBin').attr('action', '/');
+    $('#theBin').find('[name="_method"]').attr('value', 'post');
+    $('#commitBin').trigger('click');
+
+  });
+
+  setTimeout(function() {
+    $('.loading').fadeOut('slow');
+  }, 500);
+
+  $(window).bind('beforeunload', function(){
+    if (SAVED) {
+      return;
+    }
+    return 'You have unsaved work, are you sure you want to quit this page?';
+  });
+});
+
